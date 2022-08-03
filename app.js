1483edc49080571c571fc34931b1ed530d0c94e1
gary <gary.white@lodgit.com>
2022-08-03 12:52:16 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function XO(str) {
+    //code here
+}
+
+console.log(XO('xo')); // true
+console.log(XO('xxOo')); // true
+console.log(XO('xxxm')); // false
+console.log(XO('Oo')); // false
+console.log(XO('ooom')); // false
