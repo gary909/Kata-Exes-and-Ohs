f626decc3aacd9c3a74a93668de70563af46b3e6
gary <gary.white@lodgit.com>
2022-08-03 13:03:32 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 function XO(str) {
-    //code here
+    let myLower = str.toLowerCase();
+    let myXcnt = 0;
+    let myOcnt = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (myLower[i] == 'x') {
+            myXcnt = myXcnt + 1;
+        } else if (myLower[i] == 'o') {
+            myOcnt = myOcnt + 1;
+        }
+    }
+    if (myOcnt === myXcnt) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(XO('xo')); // true
